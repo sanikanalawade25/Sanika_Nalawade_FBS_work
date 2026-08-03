8e9139bdcd74bb3f59a61a1038746e4c3a9bfea8
sanikanalawade25 <sanikanalawade25@gmail.com>
2026-08-03 20:19:18 +0530

    Add files via upload
    Assignment No 7

---
+++ core python/assignments/Assignment7/Qg.py
@@ -0,0 +1,10 @@
+# Q7 write program to print following patterns
+#        
+for i in range(1,6):
+    for j in range(1,6-i):
+        print(' ',end=' ')
+    for j in range(1,i+1):
+        print(j,end=' ')
+    for j in range(i-1,0,-1):
+        print(j,end=' ')
+    print()
