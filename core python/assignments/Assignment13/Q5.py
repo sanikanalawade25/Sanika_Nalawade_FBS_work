59e70fa665d434adf4f761dde6be8c27022cd1e5
sanikanalawade25 <sanikanalawade25@gmail.com>
2026-08-17 00:21:17 +0530

    Delete core python/assignments/Assignment13 directory

--- core python/assignments/Assignment13/Q5.py
+++
@@ -1,7 +0,0 @@
-# Q5.Python Program to sum All the items in dictionary
-d={'a':10,'b':20,'c':30}
-sum=0
-for i in d:
-    sum=sum+d[i]
-print("dictionary ",d)
-print("sum of all items",sum)
--- core python/assignments/Assignment13/Q7.py
+++
@@ -1,8 +0,0 @@
-# Q7.Python Program to remove then given from dictionary
-d={'a':1,'name':'Sanika','age':21}
-key=input("Enetr Remove key:")
-if key in d:
-    d.pop(key)
-    print('Update Dictionary=',d)
-else:
-    print('key does not exist')
